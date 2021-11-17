107bd6362f75237c40be3cf49ae71c9bfa4bc35d
kuc00000 <kwchany@naver.com>
2021-11-18 03:54:33 +0900

    모델 input에 글로벌 골드 추가

--- api/prediction_model_views.py
+++ api/prediction_model_views.py
@@ -11,19 +11,21 @@ import numpy as np
 
 
 class MachineLearningModel(APIView):
-    def showMatchResult(self, dragons, barons, towers, model):
+    def showMatchResult(self, dragons, barons, towers,totalgold, model):
         num = 1
         x = np.array(dragons)
         y = np.array(barons)
         z = np.array(towers)
+        a = np.array(totalgold)
 
         if x.shape[0] != 1:
             num = x.shape[0]
-        x = np.asarray(x).astype('int32').reshape((-1, 1))
-        y = np.asarray(y).astype('int32').reshape((-1, 1))
-        z = np.asarray(z).astype('int32').reshape((-1, 1))
+        x = np.asarray(x).astype('float32').reshape((-1, 1))
+        y = np.asarray(y).astype('float32').reshape((-1, 1))
+        z = np.asarray(z).astype('float32').reshape((-1, 1))
+        a = np.asarray(a).astype('float32').reshape((-1, 1))
 
-        preds = model.predict([x, y, z])
+        preds = model.predict([x, y, z,a])
         preds = preds.reshape(num,)
         return preds
 
@@ -35,7 +37,8 @@ class MachineLearningModel(APIView):
         dragons = request.data['dragons']
         barons = request.data['barons']
         towers = request.data['towers']
+        totalgold = request.data['totalgold']
 
-        preds = self.showMatchResult(dragons, barons, towers)
+        preds = self.showMatchResult(dragons, barons, towers,totalgold)
         print(preds)
         return Response({"Data": True})  # 이 부분은 어떻게 해야 할 지 몰라서 안 건드렸음
