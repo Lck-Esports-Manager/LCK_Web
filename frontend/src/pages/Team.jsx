0b7eccd4399278701e1f286ae13503effd47b21f
JIH0LEE <skql5810@gmail.com>
2021-11-18 16:05:49 +0900

    react-bootstrap추가

--- frontend/src/pages/Team.jsx
+++ frontend/src/pages/Team.jsx
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import Maintitle from './Maintitle';
+import {Button} from 'react-bootstrap'
 
 export default function Team() {
+    const [myTeam, setMyTeam] = useState({name:"test"})
+    useEffect(() => {
+        const fetchTeam = async () => {
+            try {
+                axios.get('http://localhost:8000/api/teaminfo/'
+                ).then((response) => {
+                    setMyTeam(response.data.my_team);
+                })
+            } catch (e) { console.log(e); }
+        };
+        fetchTeam();
+    }, []);
     return (<>
         <Maintitle />
-        <div>팀관리 페이지</div>
+        <div>{myTeam.name}</div>
+        <Button variant="secondary">Secondary</Button>
     </>);
 }
