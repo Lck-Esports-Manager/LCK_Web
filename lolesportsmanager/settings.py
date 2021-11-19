e36134ebda516a89cc1426295d24db2e3b2b6856
JIH0LEE <skql5810@gmail.com>
2021-11-19 16:29:21 +0900

    주석

--- lolesportsmanager/settings.py
+++ lolesportsmanager/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import mysetting
 import os
 import datetime
-from keras.models import load_model
+#from keras.models import load_model
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
