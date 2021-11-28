ef9c9d95d0ddb9c512921b4aad5d7e5c55a800d2
JIH0LEE <skql5810@gmail.com>
2021-11-28 18:13:20 +0900

    팀관리 디자인 작업중

--- frontend/src/pages/Team.jsx
+++ frontend/src/pages/Team.jsx
@@ -5,6 +5,54 @@ import { Row, Card, Col, Button, Tabs, Tab, Container, Table } from 'react-boots
 import { header } from "../config.js";
 import './Team.css';
 
+const style1={
+    border:'none',
+    backgroundColor:'#011e46'
+}
+const style1_1={
+    border:'none',
+    backgroundColor:'#ffdba1',
+    color:'#011e46'
+}
+const style2={
+    borderRadius:"10px",
+    textAlign:'center',
+    width:'300px',
+    alignContent:"center",
+    margin:"auto",
+    marginTop:"10px",
+    marginBottom:"10px",
+    padding:"5px",
+    backgroundColor:'#011e46',
+    color:'#ffdba1'
+}
+const style3={
+    width:"600px",
+    marginTop:"50px",
+    marginBottom:"50px",
+    height:"200px",
+    borderRadius:"10px",
+    backgroundColor:'#011e46',
+    color:"white"
+}
+const style4={
+    width:"600px",
+    marginTop:"50px",
+    marginBottom:"50px",
+    height:"200px",
+}
+const style5={
+    borderRadius:"10px",
+    textAlign:'center',
+    width:'150px',
+    alignContent:"center",
+    margin:"auto",
+    marginTop:"10px",
+    marginBottom:"10px",
+    padding:"5px",
+    backgroundColor:'#011e46',
+    color:'white'
+}
 function Player({ player, pos }) {
     const history = useHistory();
     const routeChange = () => {
@@ -18,17 +66,16 @@ function Player({ player, pos }) {
     }
     return (
         <>
-            <h3 class='my-2'>
+            <h4 style={style5}>
                 {pos}
-            </h3>
-            <Card style={{ width: '12rem' }}>
+            </h4>
+            <Card style={{ width: '12rem',margin:"auto" }} onClick={routeChange}>
                 <Card.Img class='m-3' variant="top" src={`http://localhost:8000${player.player.images}`} />
                 <Card.Body>
                     <Card.Title>{player.player.name}</Card.Title>
                     <Card.Subtitle className="mb-2 text-muted">{player.player.year} {player.player.season}</Card.Subtitle>
                 </Card.Body>
             </Card>
-            <Button variant="secondary" onClick={routeChange}>상세보기</Button>
         </>
     );
 }
@@ -39,13 +86,14 @@ function TeamInfo({ myTeam }) {
         let path = '/changeteam/';
         history.push(path);
     }
+    
     return (
         <>
-            <Button variant='secondary' onClick={routeChange}>팀 수정</Button>
+            <Button style={style1} onClick={routeChange}>팀 수정</Button>
             
             
             <Container>
-                <h3  style={{border:"solid",textAlign:'center',width:'100px',alignContent:"center",margin:"auto",padding:"5px"}} >주전</h3>
+                <h3  style={style2} >주전</h3>
                 <Row className="justify-content-md-center">
                     <Col><Player player={myTeam.top} pos="Top"></Player></Col>
                     <Col><Player player={myTeam.jungle} pos="Jungle"></Player></Col>
@@ -56,10 +104,13 @@ function TeamInfo({ myTeam }) {
             </Container>
             
             <Container>
-            <h3  style={{border:"solid",textAlign:'center',width:'100px',alignContent:"center",margin:"auto", marginTop:"10px",padding:"5px"}} >서브</h3>
+            <h3  style={style2} >서브</h3>
                 <Row className="justify-content-md-center">
                     <Col><Player player={myTeam.sub1} pos="Sub1"></Player></Col>
                     <Col><Player player={myTeam.sub2} pos="Sub2"></Player></Col>
+                    <Col><div ></div></Col>
+                    <Col><div ></div></Col>
+                    <Col><div ></div></Col>
                 </Row>
             </Container>
         </>
@@ -69,15 +120,16 @@ function TeamInfo({ myTeam }) {
 function Sponsor1({ sponsor }) {
 
     if (sponsor == null) {
-        return (<div></div>)
+        return (<div style={style4}></div>)
     }
     else {
         return (
-            <Card style={{ width: '12rem' }}>
+            <Card style={style3}>
                 <Card.Body>
                     <Card.Title>{sponsor.name}</Card.Title>
                     <Card.Text className="mb-2 text-muted">{sponsor.description}</Card.Text>
-                    <Card.Text className="mb-2 text-muted">승리 시 {sponsor.earning}원 만큼 획득합니다</Card.Text>
+                    <Card.Text className="mb-2 text-muted"></Card.Text>
+                    <Card.Text className="mb-2 text-muted">승리하면 {sponsor.earning}원을 추가로 획득합니다</Card.Text>
                 </Card.Body>
             </Card>)
     }
@@ -104,12 +156,12 @@ function Sponsor2({ sponsor }) {
     }
     else {
         return (
-            <Card style={{ width: '12rem' }}>
+            <Card style={style3}>
                 <Card.Body>
                     <Card.Title>{sponsor.name}</Card.Title>
                     <Card.Text className="mb-2 text-muted">{sponsor.description}</Card.Text>
-                    <Card.Text className="mb-2 text-muted">승리 시 {sponsor.earning}원 만큼 획득합니다</Card.Text>
-                    <Button variant="secondary" onClick={StartSponsor}>계약 하기</Button>
+                    <Card.Text className="mb-2 text-muted">승리하면 {sponsor.earning}원을 추가로 획득합니다</Card.Text>
+                    <Button style={style1_1} onClick={StartSponsor}>계약 하기</Button>
                 </Card.Body>
             </Card>)
     }
@@ -121,8 +173,8 @@ function SponsorInfo({ sponsor1, sponsor2, sponsor3, _available_sponsor }) {
     return (
         <>
 
-            <h3 class='my-2'>계약 중인 스폰서</h3>
-            <Container>
+            <h3 style={style2}>계약 중인 스폰서</h3>
+            <Container style={{height:"300px"}}>
                 <Row className="justify-content-md-center">
 
                     <Col><Sponsor1 sponsor={sponsor1}></Sponsor1></Col>
@@ -131,8 +183,8 @@ function SponsorInfo({ sponsor1, sponsor2, sponsor3, _available_sponsor }) {
 
                 </Row>
             </Container>
-            <h3 class='my-2'>계약 가능한 스폰서</h3>
-            <Container>
+            <h3 style={style2}>계약 가능한 스폰서</h3>
+            <Container style={{height:"300px"}}>
                 <Row className="justify-content-md-center">
                     {_available_sponsor && _available_sponsor.map((sponsor) => (
                         <Col><Sponsor2 sponsor={sponsor}></Sponsor2></Col>
@@ -163,16 +215,17 @@ function Enterprise2({ enterprise, money }) {
     }
 
     if (enterprise == null) {
-        return (<div></div>)
+        return (<div style={style4}></div>)
     }
     else {
         return (
-            <Card style={{ width: '12rem' }}>
+            <Card style={style3}>
                 <Card.Body>
                     <Card.Title>{enterprise.name}</Card.Title>
                     <Card.Text className="mb-2 text-muted">{enterprise.description}</Card.Text>
-                    <Card.Text className="mb-2 text-muted">초기비용 : {enterprise.cost}원</Card.Text>
-                    <Button variant="secondary" onClick={StartEnterprise}>시작하기</Button>
+                    <Card.Text className="mb-2 text-muted">시작하면 {enterprise.cost}원이 소모됩니다</Card.Text>
+                    <Card.Text className="mb-2 text-muted">승리하면 {enterprise.earning}원을 추가로 획득합니다</Card.Text>
+                    <Button style={style1_1} onClick={StartEnterprise}>시작하기</Button>
                 </Card.Body>
             </Card>)
     }
@@ -185,11 +238,11 @@ function Enterprise1({ enterprise, money }) {
     }
     else {
         return (
-            <Card style={{ width: '12rem' }}>
+            <Card style={style3}>
                 <Card.Body>
                     <Card.Title>{enterprise.name}</Card.Title>
                     <Card.Text className="mb-2 text-muted">{enterprise.description}</Card.Text>
-
+                    <Card.Text className="mb-2 text-muted">승리하면 {enterprise.earning}원을 추가로 획득합니다</Card.Text>
                 </Card.Body>
             </Card>)
     }
@@ -199,15 +252,15 @@ function EnterpriseInfo({ enterprise1, enterprise2, _available_enterprise, money
     return (
         <>
 
-            <h3 class='my-2'>진행 중인 사업</h3>
-            <Container>
+            <h3 style={style2}>진행 중인 사업</h3>
+            <Container style={{height:"300px"}}>
                 <Row className="justify-content-md-center">
                     <Col><Enterprise1 enterprise={enterprise1}></Enterprise1></Col>
                     <Col><Enterprise1 enterprise={enterprise2}></Enterprise1></Col>
                 </Row>
             </Container>
-            <h3 class='my-2'>시작 가능한 사업</h3>
-            <Container>
+            <h3 style={style2}>시작 가능한 사업</h3>
+            <Container style={{height:"300px"}}>
                 <Row className="justify-content-md-center">
                     {_available_enterprise && _available_enterprise.map((enterprise) => (
                         <Col><Enterprise2 enterprise={enterprise} money={money}></Enterprise2></Col>
@@ -359,7 +412,8 @@ export default function Team() {
                         <div className="title">팀 관리</div>
                         <Tabs defaultActiveKey="team" id="uncontrolled-tab-example" className="mb-3">
                             <Tab  eventKey="team" title="로스터 정보">
-                                {data.my_team.sub1 === null || data.my_team.sub2 === null ? <Button variant ='secondary' onClick={moveAddPlayer}>선수 영입하기</Button> : <div></div>}
+                                {data.my_team.sub1 === null || data.my_team.sub2 === null ? <Button style={style1} 
+                                onClick={moveAddPlayer}>선수 영입하기</Button> : <div></div>}
                                 <TeamInfo myTeam={data.my_team}></TeamInfo>
                             </Tab>
                             <Tab eventKey="sponsor" title="스폰서">
